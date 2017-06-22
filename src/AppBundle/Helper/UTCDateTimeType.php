a193a9dd8e14d6d0e97f1d41ca8b4b757d842bc3
Sarel van der Walt <sarel.w@gmail.com>
2017-06-22 21:15:43 +0200

    Overwrote DateTimeType so I can force UTC always

---
+++ src/AppBundle/Helper/UTCDateTimeType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Helper;
+
+use DateTimeZone;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+class UTCDateTimeType extends DateTimeType
+{
+    static private $utc;
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof \DateTime) {
+            $value->setTimezone(new DateTimeZone('UTC'));
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || $value instanceof \DateTime) {
+            return $value;
+        }
+
+        $converted = \DateTime::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::$utc ? self::$utc : self::$utc = new DateTimeZone('UTC')
+        );
+
+        if (! $converted) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString()
+            );
+        }
+
+        return $converted;
+    }
+
+
+}
